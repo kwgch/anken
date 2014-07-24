ef39f786189766d13ce2fa88df4e540e3a349b5f
kwgch <m.kwgch@gmail.com>
2014-07-24 14:39:41 +0900

    change css

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
   def index
 #     @projects = Project.all
     @q = Project.search(params[:q])
-    @projects = @q.result.paginate(page: params[:page], per_page: 10)
+    @projects = @q.result.paginate(page: params[:page], per_page: 5)
   end
 
   def search
